6155093e7b18cc0ed957c37a23cf52d814616475
Martim <cruzsousamartim@gmail.com>
2025-12-15 09:34:01 +0000

    Add files via upload

---
+++ ciclowhile.py
@@ -0,0 +1,8 @@
+LInf=int(input("qual um valor limite inferior?\n"))
+LSup=int(input("qual um limite superior?\n"))
+While LSup<LInf: 
+    print("Os limites indicados para o intervalo estao corretos")
+While LInf>Lsup :
+    print("Os limites indicados estao corretos")
+
+
