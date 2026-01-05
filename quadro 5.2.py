ae48a01af6b0b6db34e4a1782a14adeff4b009ae
Martim <cruzsousamartim@gmail.com>
2026-01-05 09:25:49 +0000

    Create quadro 5.2.py

---
+++ quadro 5.2.py
@@ -0,0 +1,17 @@
+numero=[1,3,5,7,8,6,10,56,23,4,14]
+abso=abs(-4)
+n_ord=ord("A")
+caract=chr(64)
+bina=bin(8)
+quoc_rest=divmod(23,5)
+n_itens=len(numero)
+num_arred=round(245.5690,1)
+print("abs(-4)=", abso)
+print("ord(A)", n_ord)
+print("chr(64)", caract)
+print("bin(8)", bina)
+print("divmod(23,5)", quoc_rest)
+print("len(numero)", n_itens)
+print("round(245.5690,1)", num_arred)
+
+
