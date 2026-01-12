7d2c66820c2176483f37f7c820507ff5718d19a1
Martim <cruzsousamartim@gmail.com>
2026-01-12 09:42:45 +0000

    Update exercicio side.py

--- funçao/exercicio side.py
+++ funçao/exercicio side.py
@@ -4,13 +4,13 @@ def adicao(a1,a2):
 def multiplicacao(a1,a2):
     return (a1 * a2)
 
-a1=int(input("qual o valor pertendido na soma?"))
-a2=int(input("qual o valor pertendido para somar ou soma?"))   
+a1=int(input("qual o valor pertendido ? "))
+a2=int(input("qual o outro valor pertendido ? "))   
     
 reultadosoma= adicao(a1,a2)
 multiplicacaores= multiplicacao(a1,a2)
 
-print("multiplicaçao é :",multiplicacaores,"adiçao é:",reultadosoma)
+print("multiplicaçao é :",multiplicacaores,"e adiçao é:",reultadosoma)
 
 
 
