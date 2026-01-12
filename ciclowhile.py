c34af33c8f45cedfd1ca26c157222b5d1c115a4f
Martim <cruzsousamartim@gmail.com>
2026-01-12 09:31:12 +0000

    .

--- ciclowhile.py
+++
@@ -1,8 +0,0 @@
-LInf=int(input("qual um valor limite inferior?\n"))
-LSup=int(input("qual um limite superior?\n"))
-While LSup<LInf: 
-    print("Os limites indicados para o intervalo estao corretos")
-While LInf>Lsup :
-    print("Os limites indicados estao corretos")
-
-
---
+++ funçao/exercicio side.py
@@ -0,0 +1,26 @@
+a1=int(input("qual o valor pertendido na soma?"))
+a2=int(input("qual o valor pertendido para somar ou soma?"))
+m1=int(input("qual o valor pertendido para multiplicar?"))
+m2=int(input("qual o outro valor pertendido para a multiplicão?"))
+
+
+def adicao(a1,a2):
+    return a1 + a2
+    
+    
+
+
+def multiplicacao(m1,m2):
+    return m1 * m2
+    
+    
+reultadosoma= adicao(a1,a2)
+multiplicacaores= multiplicacao(m1,m2)
+
+
+
+
+print("multiplicaçao é :",multiplicacaores,"adiçao é:",reultadosoma)
+
+
+
