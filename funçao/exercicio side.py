417053557593e7692ed0deb4105b14a873bf2a57
Martim <cruzsousamartim@gmail.com>
2026-01-12 09:41:52 +0000

    Update exercicio side.py

--- funçao/exercicio side.py
+++ funçao/exercicio side.py
@@ -1,24 +1,14 @@
-a1=int(input("qual o valor pertendido na soma?"))
-a2=int(input("qual o valor pertendido para somar ou soma?"))
-m1=int(input("qual o valor pertendido para multiplicar?"))
-m2=int(input("qual o outro valor pertendido para a multiplicão?"))
-
-
 def adicao(a1,a2):
-    return a1 + a2
-    
-    
+    return (a1 + a2)
+      
+def multiplicacao(a1,a2):
+    return (a1 * a2)
 
-
-def multiplicacao(m1,m2):
-    return m1 * m2
-    
+a1=int(input("qual o valor pertendido na soma?"))
+a2=int(input("qual o valor pertendido para somar ou soma?"))   
     
 reultadosoma= adicao(a1,a2)
-multiplicacaores= multiplicacao(m1,m2)
-
-
-
+multiplicacaores= multiplicacao(a1,a2)
 
 print("multiplicaçao é :",multiplicacaores,"adiçao é:",reultadosoma)
 
