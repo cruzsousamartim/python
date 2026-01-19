9ac0269925c50c642325b04a3067c1614578e400
Martim <cruzsousamartim@gmail.com>
2026-01-19 08:17:00 +0000

    Update raiz.py

---
+++ exercicios/raiz.py
@@ -0,0 +1,8 @@
+import math
+
+A = float(input("Qual o numero desejado em A? "))
+B = float(input("Qual o numero desejado em B? "))
+
+c = math.sqrt(A + B)
+
+print("O resultado da sua operação é:", c)
