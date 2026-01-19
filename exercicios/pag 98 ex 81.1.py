5fbb1638ad033252d195191bb4d94af64b6d0750
Martim <cruzsousamartim@gmail.com>
2026-01-19 09:12:36 +0000

    .

---
+++ exercicios/pag 98 ex 81.1.py
@@ -0,0 +1,10 @@
+import turtle
+screen=turtle.Screen()
+tri= turtle.Turtle()
+
+for _ in range(3):
+    tri.forward(100)
+    tri.left(120)
+    
+turtle.done()
+    
---
+++ exercicios/pag 98 ex 81.2.py
@@ -0,0 +1,8 @@
+import turtle
+desenho= turtle.Turtle
+lado=int( input ("valor do lado:" ))
+for n in range(4):
+    desenho.forward(90)
+    desenho.left(90)
+    
+turtle.done()
